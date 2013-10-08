906f82d41566e6cf5f734ea674de110cb1fabb9a
Mamba <mambax7@gmail.com>
2013-10-08 00:42:16 -0400

    Adding Boox 1.71 Final

---
+++ boox/language/english/admin.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ****************************************************************************
+ * boox - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @package         boox
+ * @author 			Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+
+define("_AM_BOOX_DBUPDATED","The database was successfully updated");
+define("_AM_BOOX_GENERALSET","Module's options");
+define("_AM_BOOX_MODULEADMIN","Module's admin");
+define("_AM_BOOX_EDIT","Edit");
+define("_AM_BOOX_DELETE","Delete");
+define("_AM_BOOX_ADD","Add");
+define("_AM_BOOX_CONFIRM","Are you sure ?");
+define("_AM_BOOX_FILES","Files");
+define("_AM_BOOX_FILE","File");
+define("_AM_BOOX_FILENAME","Filename");
+define("_AM_BOOX_ACTION","Action");
+define("_AM_BOOX_COMMAND","Command to include in your theme");
+define("_AM_BOOX_ERROR1","Please specify a filename");
+define("_AM_BOOX_ERROR2","Error, unable to save the file %s");
+define("_AM_BOOX_ERROR3","Error, unable to read the file %s");
+define("_AM_BOOX_CONTENT","Content");
+
+//ModuleAdmin
+define('_AM_BOOX_MODULEADMIN_MISSING','Error: The ModuleAdmin class is missing. Please install the ModuleAdmin Class into /Frameworks (see /docs/readme.txt)');
